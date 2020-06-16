e6cdf4031775f722c76c55a0f03ec7d3d1fbd7b6
frank_glb <frank_glb@swu.edu.cn>
2020-06-16 12:02:18 +0800

    commit GitCaseTest—hotfix

--- src/main/java/edu/swu/git/Apple.java
+++ src/main/java/edu/swu/git/Apple.java
@@ -5,6 +5,8 @@ public class Apple {
 	public static void main(String[] args) {
 		System.out.println("right...");
 		System.out.println("left...");
+		
+		System.out.println("bug–ﬁ∏¥ÕÍ≥…£°");
 	}
 	
 }
