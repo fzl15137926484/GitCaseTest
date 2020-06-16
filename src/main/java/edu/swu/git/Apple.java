2559819b8c7718110f7ba5bd01dab18ddc400762
frank_glb <frank_glb@swu.edu.cn>
2020-06-16 11:53:13 +0800

    commit conflict

--- src/main/java/edu/swu/git/Apple.java
+++ src/main/java/edu/swu/git/Apple.java
@@ -4,9 +4,6 @@ public class Apple {
 	
 	public static void main(String[] args) {
 		System.out.println("right...");
-	}
-
-	public static void main(String[] args) {
 		System.out.println("left...");
 	}
 	
